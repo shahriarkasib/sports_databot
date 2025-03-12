9713f2ee46b807f1efaccb36a391068e5ed1ae0a
shahriar Sourav <shahriarsourav2905@gmail.com>
2025-03-12 23:46:54 +0600

    first commit

--- postgre_connection.py
+++ postgre_connection.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from sqlalchemy import inspect
 import pandas as pd
 
-database_url = 'postgresql+psycopg2://u6t22h6qa851ge:pdd922f62af664ee296fdfcfaaf39ba95d3fcecddd6714fc69e029da8bfaa86e5@c4g0h0kljo97tk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dequvcgggnb24g'
+database_url = 'database url'
 
 # Create the engine using the database URL
 engine = sqlalchemy.create_engine(database_url)
